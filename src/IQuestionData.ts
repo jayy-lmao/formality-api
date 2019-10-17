c4b52887b2afee51ca7f19d958e65d102d07586b
Jayy-Lmao <jameswarringtonholman@gmail.com>
2019-10-17 23:06:38 +1100

    Forms and Questions are now linked up

--- src/IQuestionData.ts
+++ src/IQuestionData.ts
@@ -1,6 +1,6 @@
 export interface IQuestionData {
-    id: number;
-    formId: number;
-    text: string;
-    questionType: "short_answer" | "multiple_choice";
+  id: number;
+  formId: number;
+  text: string;
+  questionType: "short_answer" | "multiple_choice";
 }
--- src/data.ts
+++ src/data.ts
@@ -2,18 +2,18 @@ import { IFormData } from "./IFormData";
 import { IQuestionData } from "./IQuestionData";
 
 export const forms: IFormData[] = [
-    {
-        description: "jklas fdakjkaf kjfaldfdajk",
-        id: 1,
-        title: "Hellaosd",
-    },
+  {
+    description: "jklas fdakjkaf kjfaldfdajk",
+    id: 1,
+    title: "Hellaosd"
+  }
 ];
 export const questions: IQuestionData[] = [
-    {
-        // form: 1,
-        formId: 1,
-        id: 1,
-        questionType: "short_answer",
-        text: "What is the speed of an unladen swallow",
-    },
+  {
+    // form: 1,
+    formId: 1,
+    id: 1,
+    questionType: "short_answer",
+    text: "What is the speed of an unladen swallow"
+  }
 ];
