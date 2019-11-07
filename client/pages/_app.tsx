2b6cdd0ed3ceea0fe4a87a0e7a558eb67f45c0a1
Jayy-Lmao <jameswarringtonholman@gmail.com>
2019-11-07 11:28:32 +1100

    progess bar

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,16 +1,31 @@
 import App, { Container } from "next/app";
 import Header from "../components/Header";
+import { ApolloProvider, Context } from "react-apollo";
+import withData from "../lib/withData";
+import React, { Component } from "react";
+
 
 class formalityApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <div>
-        <Header />
-        <Component {...pageProps} />
-      </div>
-    );
-  }
+    static async getInitialProps({ Component, ctx}: {Component: Component, ctx: Context}) {
+        let pageProps = {};
+        if ((Component as any).getInitialProps) {
+            pageProps = await (Component as any).getInitialProps(ctx);
+        }
+        (pageProps as any).query = ctx.query;
+        return { pageProps };
+    }
+
+    render() {
+        const { Component, pageProps, apollo } = this.props;
+        return (
+            <div>
+                <ApolloProvider client={apollo}>
+                    <Header />
+                    <Component {...pageProps} />
+                </ApolloProvider>
+            </div>
+        );
+    }
 }
 
 export default formalityApp;
