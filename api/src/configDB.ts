a892734df14bee2375b58c411b93d34a46f4f5ec
Jayy-Lmao <jameswarringtonholman@gmail.com>
2019-11-14 17:10:25 +1100

    Still solving no apply undefined issues

--- api/src/configDB.ts
+++ api/src/configDB.ts
@@ -1,29 +1,29 @@
 import { createConnection } from "typeorm";
 
 export const configDB = async () => {
-    if (process.env.TYPEORM_USERNAME) {
-        await createConnection({
-            database: process.env.TYPEORM_DATABASE,
-            entities: [ "src/schemas/*.ts" ],
-            host: process.env.TYPEORM_HOST,
-            logging: true,
-            password: process.env.TYPEORM_PASSWORD,
-            port: 27017,
-            ssl: false,
-            synchronize: true,
-            type: "mongodb",
-            username: process.env.TYPEORM_USERNAME,
-        });
-    } else {
-        await createConnection({
-            database: "typegraphql-formality",
-            entities: [ "src/schemas/*.ts" ],
-            logging: true,
-            ssl: true,
-            synchronize: true,
-            type: "mongodb",
-            url: process.env.TYPEORM_HOST,
-            useUnifiedTopology: true,
-        });
-    }
+  if (process.env.TYPEORM_USERNAME) {
+    await createConnection({
+      database: process.env.TYPEORM_DATABASE,
+      entities: ["src/schemas/*.ts"],
+      host: process.env.TYPEORM_HOST,
+      logging: true,
+      password: process.env.TYPEORM_PASSWORD,
+      port: 27017,
+      ssl: false,
+      synchronize: true,
+      type: "mongodb",
+      username: process.env.TYPEORM_USERNAME
+    });
+  } else {
+    await createConnection({
+      database: "typegraphql-formality",
+      entities: ["src/schemas/*.ts"],
+      logging: true,
+      ssl: true,
+      synchronize: true,
+      type: "mongodb",
+      url: process.env.TYPEORM_HOST,
+      useUnifiedTopology: true
+    });
+  }
 };
